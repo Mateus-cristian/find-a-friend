bd4b395922bfda2a7b373d94a87a93769c92e047
mateus-cristian <mateus_cristian@outlook.com>
2024-02-20 20:16:57 -0300

    feat:config prisma

---
+++ prisma/migrations/20240220231120_/migration.sql
@@ -0,0 +1,36 @@
+-- CreateTable
+CREATE TABLE "pets" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "type" TEXT NOT NULL,
+    "size" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "energy_level" INTEGER NOT NULL,
+    "dependency_level" TEXT NOT NULL,
+    "environment" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "latitude" DECIMAL(65,30),
+    "longitude" DECIMAL(65,30),
+    "organization_id" TEXT NOT NULL,
+
+    CONSTRAINT "pets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "organization " (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "cep" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "latitude" DECIMAL(65,30),
+    "longitude" DECIMAL(65,30),
+    "whatsapp" INTEGER NOT NULL,
+    "password_hash" TEXT NOT NULL,
+
+    CONSTRAINT "organization _pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "pets" ADD CONSTRAINT "pets_organization_id_fkey" FOREIGN KEY ("organization_id") REFERENCES "organization "("id") ON DELETE RESTRICT ON UPDATE CASCADE;
